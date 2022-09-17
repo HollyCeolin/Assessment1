c4580d1509006019645ef33f1fb00e3399e94811
Holly Ceolin <103793775+HollyCeolin@users.noreply.github.com>
2022-09-16 23:22:52 -0400

    Update password.js
    I don't feel it is complete, I will keep working on it.

--- password.js
+++ password.js
@@ -1,15 +1,29 @@
-const numArray = [0,1,2,3,4,5,6,7,8,9];
-let password = "Orangeoctopus3!";
-const alphabetLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-let includesUpper = false;
-let includesLower = false;
-for (let i = 0, i< password.length ; i++){
+//const numArray = [0,1,2,3,4,5,6,7,8,9];
+const password = "Orangeoctopus3!";
+//const alphabetLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+//const alphabetUppercase = ['A', "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+//let includesUpper = false;
+//let includesLower = false;
 
-    let each_letter = password[i];
-    if (alphabetLowercase.includes((each_letter))) {
-        includesLower = true
-        includesUpper= true
+for(let i = 0; i <= password.length - 1; i++){
+   
+    if(password.length < 10){
+        alert ("Password must contain 10 characters.")
+       
+    } else if (password.search(/[a-z]/) < 0) {
+        alert("Your password needs a lower case letter")
+        return false;
+    } else if(password.search(/[A-Z]/) < 0) {
+        alert("Your password needs an uppser case letter");
+        return false;
+    } else if (password.search(/[0-9]/) < 0) {
+        alert("Your password needs a number");
+        return false;
+    } else if (password.search(/[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/) < 0){
+        alert("Your password needs a special character.");
+        return false;
+    }else{
+        return true;
     
-    }
-    if (includesLower)
+    }console.log("Your password meets all the requirements!")
 }
